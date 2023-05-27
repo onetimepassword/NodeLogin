5591c7445429decc497f6dbbc0bb62a26441fe32
stephan <stephan.steyn@gmail.com>
2023-05-27 12:05:16 +0000

    redirect

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello, Express!");
+  // Redirect to an alternate port (e.g., 3000)
+  const redirectUrl = `http://${req.hostname}:3000${req.url}`;
+  res.redirect(redirectUrl);
 });
 
 let port = 8080
